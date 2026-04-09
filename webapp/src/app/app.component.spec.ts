d9c85ab5d30aff6d78839619d2311d253c3ce128
Etienne Noel <etiennenoel@gmail.com>
2026-04-08 21:14:09 -0700

    update

--- webapp/src/app/app.component.spec.ts
+++ webapp/src/app/app.component.spec.ts
@@ -2,13 +2,17 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { AppUpdateService } from './core/services/app-update.service';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AppComponent],
       imports: [RouterModule.forRoot([])],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA]
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      providers: [
+        { provide: AppUpdateService, useValue: { init: () => {} } }
+      ]
     }).compileComponents();
   });
 
