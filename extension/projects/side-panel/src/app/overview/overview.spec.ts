8fc6a38c664edba1daca09510c1e5cd3058a4ece
Etienne Noel <etiennenoel@gmail.com>
2026-05-04 15:26:53 -0700

    -update

--- extension/projects/side-panel/src/app/overview/overview.spec.ts
+++ extension/projects/side-panel/src/app/overview/overview.spec.ts
@@ -2,20 +2,13 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { OverviewComponent } from './overview';
 import { CommonModule } from '@angular/common';
 import { NO_ERRORS_SCHEMA, NgZone } from '@angular/core';
-import {
-    ModelManager,
-    WriterManager,
-    RewriterManager,
-    PromptManager,
-    ProofreaderManager,
-    SummarizerManager,
-    TranslatorManager,
-    LanguageDetectorManager
-} from 'base';
-import { of } from 'rxjs';
+import { DiagnosisService } from 'base';
+import { of, BehaviorSubject } from 'rxjs';
 
-class MockManager {
-  modelDownloadedEvent = of(null);
+class MockDiagnosisService {
+  apis$ = new BehaviorSubject<any[]>([]);
+  errorCount$ = new BehaviorSubject<number>(0);
+  runChecks() {}
 }
 
 describe('OverviewComponent', () => {
@@ -27,14 +20,7 @@ describe('OverviewComponent', () => {
       declarations: [OverviewComponent],
       imports: [CommonModule],
       providers: [
-        { provide: ModelManager, useClass: MockManager },
-        { provide: WriterManager, useClass: MockManager },
-        { provide: RewriterManager, useClass: MockManager },
-        { provide: PromptManager, useClass: MockManager },
-        { provide: ProofreaderManager, useClass: MockManager },
-        { provide: SummarizerManager, useClass: MockManager },
-        { provide: TranslatorManager, useClass: MockManager },
-        { provide: LanguageDetectorManager, useClass: MockManager },
+        { provide: DiagnosisService, useClass: MockDiagnosisService }
       ],
       schemas: [NO_ERRORS_SCHEMA]
     }).compileComponents();
@@ -43,18 +29,15 @@ describe('OverviewComponent', () => {
     component = fixture.componentInstance;
     
     // Mock the window.chrome and window.LanguageModel if necessary
-    (window as any).chrome = { runtime: { onMessage: { addListener: () => {} } } };
+    (window as any).chrome = { 
+      runtime: { onMessage: { addListener: () => {} } },
+      tabs: { onActivated: { addListener: () => {} }, onUpdated: { addListener: () => {} }, query: () => {} }
+    };
     
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
-  });
-
-  it('should initialize apiCapabilities to empty and then fetch', async () => {
-    expect(component.apiCapabilities).toBeDefined();
-    await component.refreshApis();
-    expect(component.apiCapabilities.length).toBeGreaterThan(0);
   });
 });
